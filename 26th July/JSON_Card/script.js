1f4ecfcf1214dff9a5e8a30fefe5003af4b0cdd3
Nandini Jamwal <nandinirjamwal@gmail.com>
2024-07-29 11:20:42 +0530

    commit

--- 26th July/JSON_Card/script.js
+++ 26th July/JSON_Card/script.js
@@ -1,72 +1,86 @@
-const res =fetch('https://dummyjson.com/recipes');
+console.log("Fetching Data... 3");
+const res = fetch('https://dummyjson.com/recipes');
 
-res.then((data)=>
-{
-    const obj=data.json();
-    obj.then((js)=>
-    {
-        // console.log(js);
-        renderUI(js);
+let allRecipes = [];
+
+res.then((data) => {
+    const obj = data.json();
+    obj.then((js) => {
+        allRecipes = js.recipes;
+        renderUI(allRecipes);
     })
-    .catch((err1)=>{
+    .catch((err1) => {
         console.log("Error with parsing", err1);
-    })
-
+    });
 })
-.catch((err)=>{
+.catch((err) => {
     console.log("ERROR", err);
 });
 
-
-
-function renderUI(js){
-    const recipe = js.recipes;
-    console.log(recipe);
+function renderUI(recipes) {
     const parent = document.getElementById("root");
-    let i=0;
-    for (i =0; i<30;i++)
-    {
-        const rec = recipe[i];
-        const nam =rec.name;
-        const ratin =rec.rating;
-        const cui =rec.cuisine;
-        const im =rec.image;
+    parent.innerHTML = ''; 
 
-        //CREATE ELEMENTS
+    recipes.forEach(rec => {
+        const { name, rating, cuisine, image } = rec;
+
+        // CREATE ELEMENTS
         const heading = document.createElement('h2');
         const divi = document.createElement('div');
         const head4 = document.createElement('h3');
         const para = document.createElement('h3');
-        const image = document.createElement('img');
-        const bu=document.createElement('button');
+        const img = document.createElement('img');
+        const btn = document.createElement('button');
 
-        //CLASS NAME
+        // CLASS NAME
         heading.className = 'head1';
         divi.className = 'divi';
         head4.className = 'head2';
         para.className = 'head2';
-        image.className = 'photo';
-        bu.className='btn';
+        img.className = 'photo';
+        btn.className = 'btn';
 
-        //ASSIGNING
-        heading.innerText = nam;
-        para.innerText= "Ratings:" +  ratin;
-        head4.innerText="Cuisine:" +  cui;
-        image.src=im;
-        bu.innerText="ORDER!";
+        // ASSIGNING
+        heading.innerText = name;
+        para.innerText = "Ratings: " + rating;
+        head4.innerText = "Cuisine: " + cuisine;
+        img.src = image;
+        btn.innerText = "ORDER!";
 
-        //ORDER EVENT
-        bu.onclick = function() {
+        // ORDER EVENT
+        btn.onclick = function() {
             window.location.href = './btn.html';
         };
 
-        //APPEND
+        // APPEND
         divi.appendChild(heading);
-        divi.appendChild(image);
+        divi.appendChild(img);
         divi.appendChild(head4);
         divi.appendChild(para);
-        divi.appendChild(bu);
+        divi.appendChild(btn);
         parent.appendChild(divi);
-    }
-
+    });
 }
+
+// Debounce function to limit the rate at which the search function is called
+function debounce(func, delay) {
+    let debounceTimer;
+    return function() {
+        const context = this;
+        const args = arguments;
+        clearTimeout(debounceTimer);
+        debounceTimer = setTimeout(() => func.apply(context, args), delay);
+    };
+}
+
+function handleSearch(event) {
+    const query = event.target.value.toLowerCase();
+    const filteredRecipes = allRecipes.filter(recipe =>
+        recipe.name.toLowerCase().includes(query)
+    );
+    renderUI(filteredRecipes);
+}
+
+// Attach the debounced search handler to the input event
+const searchBar = document.getElementById('searchBar');
+searchBar.addEventListener('input', debounce(handleSearch, 300));
